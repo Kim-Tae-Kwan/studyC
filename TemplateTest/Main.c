910a9bca1f5f78a5a4208ffd6a64bc972bb1830c
KimTaeKwan <vodjaos@gmail.com>
2020-01-31 15:11:14 +0900

    3

--- TemplateTest/Main.c
+++ TemplateTest/Main.c
@@ -12,6 +12,16 @@ int main()
     printf("%.1lf\n",3.141592);
     printf("%.10lf\n",3.14);
     printf("%.1lf + %.1lf = %.1lf\n", 10.2, 3.4, 10.2 + 3.4);
+
+    printf("%lf\n", 1e10);
+    printf("%.6lf\n", 3.14e-5);
+    printf("%.2le\n", 0.0000314);
+    printf("%.1le\n", 0.0000314);
+    printf("%c\n", 'c');
+    printf("%s\n", "zzzzzzzzzzzzz");
+    printf("%d\n", 'a');
+
+
     
     //type here
     
