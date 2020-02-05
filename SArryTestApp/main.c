2c2bbbe4c54011d224d90df46daf202d3283a409
KimTaeKwan <vodjaos@gmail.com>
2020-02-05 17:20:45 +0900

    14장 진행

---
+++ SArryTestApp/main.c
@@ -0,0 +1,37 @@
+/*
+  filename - main.c
+  version - 1.0
+  description - 기본 메인 함수 / 2차원 배열 학습
+  --------------------------------------------------------------------------------
+  first created - 2020.02.05
+  writer - KimTK
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// 메인함수
+int main(void)
+{
+	char score[5][20];
+
+    int i;
+    int count;
+
+    count = sizeof(score) / sizeof(score[0]);
+    printf("%d %d\n", sizeof(score), sizeof(score[0]));
+    for (i = 0; i < count; i++)
+    {
+        scanf("%s", score[i]);
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        printf("%s", score[i]);
+    }
+    printf("\n");
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
